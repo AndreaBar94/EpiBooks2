afa4e0d5588d9578b7f659560d00141c3cc8f90b
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-03-30 16:38:17 +0200

    search ok

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -10,6 +10,7 @@ import SingleBook from "./SingleBook";
 class BookList extends Component {
     state = {
         initialValue: null,
+        searchString: "",
         allTheBooks: [
             ...scifi,
             ...fantasy,
@@ -21,70 +22,44 @@ class BookList extends Component {
     handleChange = (event) => {
         this.setState({initialValue: event.target.value});
     }
+    filterBookList = (event) =>{
+        this.setState({searchString: event.target.value});
+    }
     render(){
-        return(
+        const {allTheBooks, searchString, initialValue} = this.state;
+
+        const filteredBooks = allTheBooks.filter(book =>
+            book.title.toLowerCase().includes(searchString.toLowerCase()) &&
+            (initialValue === null || initialValue === book.category.toLowerCase())
+            );
+
+            return(
             <>
-            <div className="bg-warning bg-opacity-25 p-5">
-            <Form>
-                <FormControl 
+                <div className="bg-warning bg-opacity-25 p-5">
+                <Form>
+                    <FormControl 
                     type="text" 
                     placeholder="Select a category (fantasy, history, horror, romance, scifi)" 
-                    onChange={this.handleChange} />
-            </Form>
-            </div>
-            
+                    onChange={this.handleChange} 
+                    />
+                    <FormControl 
+                    type="text" 
+                    className="my-3"
+                    placeholder="Search by title" 
+                    onChange={this.filterBookList} 
+                    />
+                </Form>
+                </div>
 
-            {this.state.initialValue === 'scifi' && (
-            <Row className="bg-warning bg-opacity-50 p-5">
-                {scifi.map((scifi) => (
-                <Col key={`scifi-book-${scifi.asin}`} className="col-12 col-md-4 col-lg-3 col-xl-2 pb-4">
-                    <SingleBook img={scifi.img} title={scifi.title} price={scifi.price}/>
-                </Col>
+                <Row className="bg-warning bg-opacity-50 p-5">
+                {filteredBooks.map((book) => (
+                    <Col key={`book-${book.asin}`} className="col-12 col-md-4 col-lg-3 col-xl-2 pb-4">
+                    <SingleBook img={book.img} title={book.title} price={book.price}/>
+                    </Col>
                 ))}
-            </Row>
-            )}
-
-            {this.state.initialValue === 'fantasy' && (
-            <Row className="bg-warning bg-opacity-50 p-5">
-                {fantasy.map((fantasy) => (
-                <Col key={`fantasy-book-${fantasy.asin}`} className="col-12 col-md-4 col-lg-3 col-xl-2 pb-4">
-                    <SingleBook img={fantasy.img} title={fantasy.title} price={fantasy.price}/>
-                </Col>
-                ))}
-            </Row>
-            )}
-
-            {this.state.initialValue === 'history' && (
-            <Row className="bg-warning bg-opacity-50 p-5">
-                {history.map((history) => (
-                <Col key={`history-book-${history.asin}`} className="col-12 col-md-4 col-lg-3 col-xl-2 pb-4">
-                    <SingleBook img={history.img} title={history.title} price={history.price}/>
-                </Col>
-                ))}
-            </Row>
-            )}
-
-            {this.state.initialValue === 'horror' && (
-            <Row className="bg-warning bg-opacity-50 p-5">
-                {horror.map((horror) => (
-                <Col key={`horror-book-${horror.asin}`} className="col-12 col-md-4 col-lg-3 col-xl-2 pb-4">
-                    <SingleBook img={horror.img} title={horror.title} price={horror.price}/>
-                </Col>
-                ))}
-            </Row>
-            )}
-
-            {this.state.initialValue === 'romance' && (
-            <Row className="bg-warning bg-opacity-50 p-5">
-                {romance.map((romance) => (
-                <Col key={`romance-book-${romance.asin}`} className="col-12 col-md-4 col-lg-3 col-xl-2 pb-4">
-                    <SingleBook img={romance.img} title={romance.title} price={romance.price}/>
-                </Col>
-                ))}
-            </Row>
-            )}
+                </Row>
             </>
-        )
+    )
     }
 }
 
